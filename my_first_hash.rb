887462537394db8f47da61a72b431265fe9f477b
FutureScript <fernandobaxter1@gmail.com>
2018-09-28 02:06:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -45,3 +45,37 @@ end
 
 
 
+def my_hash
+  my_hash = {"a number" => 8}
+end
+
+
+def shipping_manifest
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+end
+
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+
+  shipping_manifest["oil paintings"]
+
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+
+  shipping_manifest['muskets'] = 2
+
+  shipping_manifest['gun powder'] = 4
+
+  shipping_manifest
+
+end
